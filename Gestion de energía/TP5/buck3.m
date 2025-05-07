17c360b0d78ef14d16b8815fc88d879b6e329b15
marianomorel <mariano.morel@live.com.ar>
2025-05-07 12:07:53 -0300

    examensxsd

--- Gestion de energía/TP5/buck3.m
+++ Gestion de energía/TP5/buck3.m
@@ -22,16 +22,11 @@ Qload = R / sqrt(L / C);
 Qloss = sqrt(L / C) / (Resr + Rs);
 Q = Qload * Qloss / (Qload + Qloss);
 
-% --- Mostrar datos clave ---
-disp("Frecuencia central (w0) [rad/s]:"); disp(w0);
-disp("Cero ESR (wesr) [rad/s]:"); disp(wesr);
-disp("Factor de calidad Q:"); disp(Q);
-
 % --- Definición de variable simbólica s ---
 s = tf('s');
 
 % --- Función de transferencia del conversor Gvd(s) ---
-Gvd = (Vg / VM) * (1 + s / wesr) / (1 + (1/Q)*(s/w0) + (s/w0)^2);
+Gvd = Vg * (1 + s / wesr) / (1 + (1/Q)*(s/w0) + (s/w0)^2);
 
 % --- Rango de frecuencia ---
 fmin = 10;
@@ -73,11 +68,13 @@ if isfinite(f_cp)
 end
 
 % --- Parámetros del controlador ---
-phi_m = deg2rad(Pm);  % Margen de fase deseado
 Tu0 = (H / VM) * Vg;
 f0 = w0 / (2*pi);
 f_p2 = 16e6;
 f_I = 2.1 * f_cp;
+
+% --- Grados a radianes ---
+phi_m = deg2rad(Pm);  % Margen de fase deseado
 
 % Cálculo de f_z y f_p1
 f_z  = f_cp * sqrt((1 - sin(phi_m)) / (1 + sin(phi_m)));
@@ -94,7 +91,6 @@ Gc0 = (1 / Tu0) * (f_cp / f0)^2 * sqrt(f_z / f_p1);
 
 % Definición del controlador Gc(s)
 Gc = Gc0 * ((1 + w_I/s) * (1 + s/w_z)) / ((1 + s/w_p1) * (1 + s/w_p2));
-disp('Gc(s) ='); disp(Gc);
 
 % --- Bode de Gc(s) ---
 [mag_Gc, phase_Gc, w_Gc] = bode(Gc, {2*pi*fmin, 2*pi*fmax});
@@ -117,7 +113,7 @@ ylabel('Fase (°)');
 xlim([fmin, fmax]);
 
 % --- Producto en lazo abierto ---
-Gol = Gvd * Gc;
+Gol = (H/VM) * Gvd * Gc;
 
 % --- Bode de lazo abierto ---
 [mag_Gol, phase_Gol, w_Gol] = bode(Gol, {2*pi*fmin, 2*pi*fmax});
@@ -138,10 +134,10 @@ xlabel('Frecuencia (Hz)');
 ylabel('Magnitud (dB)');
 title('Bode de lazo abierto G_{ol}(s) = G_{vd}(s) * G_c(s)');
 xlim([fmin, fmax]);
-if isfinite(f_cg_ol)
-  plot(f_cg_ol, 0, 'ro');
-  text(f_cg_ol, 5, sprintf('GM = %.2f dB @ %.1f Hz', Gm_ol_dB, f_cg_ol), 'Color', 'red');
-end
+%if isfinite(f_cg_ol)
+  %plot(f_cg_ol, 0, 'ro');
+  %text(f_cg_ol, 5, sprintf('GM = %.2f dB @ %.1f Hz', Gm_ol_dB, f_cg_ol), 'Color', 'red');
+%end
 
 subplot(2,1,2);
 semilogx(f_Gol, squeeze(phase_Gol), 'k', 'LineWidth', 2);
@@ -153,4 +149,94 @@ if isfinite(f_cp_ol)
   plot(f_cp_ol, -180 + Pm_ol, 'ro');
   text(f_cp_ol, -180 + Pm_ol + 10, sprintf('PM = %.1f° @ %.1f Hz', Pm_ol, f_cp_ol), 'Color', 'red');
 end
+
+% --- Definir impedancias de salida ---
+Z1 = R;
+Z2 = 1/(s*C) + Resr;
+Z3 = Rs + s*L;
+Z_out_ol = 1/(1/Z1 + 1/Z2 + 1/Z3);
+Z_out_cl = Z_out_ol / (1 + Gol);
+
+% --- Rango de frecuencia para Bode ---
+w_min = 2*pi*fmin;
+w_max = 2*pi*fmax;
+
+% --- Calcular Bode de ambas funciones ---
+[mag_ol, phase_ol, w] = bode(Z_out_ol, {w_min, w_max});
+[mag_cl, phase_cl, ~] = bode(Z_out_cl, w);
+
+f = w / (2*pi);  % Convertir a Hz
+
+% --- Gráficos de Bode ---
+figure(4); clf;
+
+% Magnitud
+subplot(2,1,1);
+semilogx(f, 20*log10(squeeze(mag_ol)), 'b', 'LineWidth', 2);
+hold on;
+semilogx(f, 20*log10(squeeze(mag_cl)), 'g', 'LineWidth', 2);
+grid on;
+title('Impedancia de salida Z_{out}(s): Lazo Abierto vs. Cerrado');
+ylabel('Magnitud (dB)');
+legend('Z_{out\_ol}', 'Z_{out\_cl}');
+xlim([fmin, fmax]);
+ylim([-60, 20]);
+
+% Fase
+subplot(2,1,2);
+semilogx(f, squeeze(phase_ol), 'b', 'LineWidth', 2);
+hold on;
+semilogx(f, squeeze(phase_cl), 'g', 'LineWidth', 2);
+grid on;
+xlabel('Frecuencia (Hz)');
+ylabel('Fase (°)');
+legend('Z_{out\_ol}', 'Z_{out\_cl}');
+xlim([fmin, fmax]);
+ylim([-180, 180]);
+
+disp('Funciones Z_out_ol(s) y Z_out_cl(s) calculadas y graficadas.');
+
+% --- Mostrar todos los parámetros y variables al final ---
+disp(' ');
+disp('--- Parámetros dados ---');
+disp(['L = ', num2str(L), ' H']);
+disp(['Rs = ', num2str(Rs), ' Ohm']);
+disp(['C = ', num2str(C), ' F']);
+disp(['Resr = ', num2str(Resr), ' Ohm']);
+disp(['Vg = ', num2str(Vg), ' V']);
+disp(['R = ', num2str(R), ' Ohm']);
+disp(['VM = ', num2str(VM), ' V']);
+disp(['Vref = ', num2str(Vref), ' V']);
+disp(['VREG = ', num2str(VREG), ' V']);
+disp(['H = ', num2str(H)]);
+
+disp(' ');
+disp('--- Variables calculadas ---');
+disp(['fesr = ', num2str(wesr/(2*pi)), ' Hz']);
+disp(['f0 = ', num2str(w0/(2*pi)), ' Hz']);
+disp(['Qload = ', num2str(Qload)]);
+disp(['Qloss = ', num2str(Qloss)]);
+disp(['Q = ', num2str(Q)]);
+
+disp(' ');
+disp('--- Margenes de estabilidad de Gvd(s) ---');
+disp(['(Gm) = ', num2str(Gm_dB), ' dB']);
+disp(['Frecuencia de GM (f_cg) = ', num2str(f_cg), ' Hz']);
+disp(['(Pm) = ', num2str(Pm), ' grados']);
+disp(['Frecuencia de PM (f_cp) = ', num2str(f_cp), ' Hz']);
+
+disp(' ');
+disp('--- Parámetros del controlador ---');
+disp(['Tu0 = ', num2str(Tu0)]);
+disp(['f_I = ', num2str(f_I), ' Hz']);
+disp(['f_z = ', num2str(f_z), ' Hz']);
+disp(['f_p1 = ', num2str(f_p1), ' Hz']);
+disp(['f_p2 = ', num2str(f_p2), ' Hz']);
+disp(['phi_m = ', num2str(rad2deg(phi_m)), ' grados']);
+disp(['Gc0 = ', num2str(Gc0)]);
+
+disp(' ');
+disp('--- Margenes de estabilidad del sistema en lazo abierto ---');
+disp(['(Pm_T) = ', num2str(Pm_ol), ' grados']);
+disp(['Frecuencia PM_T) = ', num2str(f_cp_ol), ' Hz']);
 
